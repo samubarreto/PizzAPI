909855d9293edc5ae488ae59022801be5678b619
Samuel Pereira Barreto <samuel.barreto@interfocus.com.br>
2024-11-13 10:07:24 -0300

    dtos adicionadas, falta fazer o pedido

---
+++ PizzAPI.Backend/src/dtos/Pizza.ts
@@ -0,0 +1,18 @@
+import { TipoMassaPizza } from "../enums/TipoMassaPizza"
+import { TipoRecheioBorda } from "../enums/TipoRecheioBorda"
+import { TipoTamanhoPizza } from "../enums/TipoTamanhoPizza"
+
+export type Pizza = {
+  id: number,
+  nome: string,
+  descricao: string,
+  ingredientes: Array<string>,
+  preco: number,
+  peso: number,
+  recheioBorda?: TipoRecheioBorda,
+  tamanho: TipoTamanhoPizza,
+  massa: TipoMassaPizza,
+  disponivel: boolean,
+  criadoEm: Date,
+  atualizadoEm: Date,
+}
