4f76a94f9b4fce18d56948801703bfa36a949ec2
Samuel Barreto <samuel.barreto@interfocus.com.br>
2025-05-21 16:02:38 -0300

    adicionado npm i ao bat de start tudo

--- start-all.bat
+++ start-all.bat
@@ -1,3 +1,3 @@
-start cmd /k "cd PizzAPI.Backend && npm run dev"
-start cmd /k "cd PizzAPI.Frontend && npm run dev"
-start cmd /k "cd PizzAPI.E2E && npm run cy:run"
+start cmd /k "cd PizzAPI.Backend && npm i && npm run dev"
+start cmd /k "cd PizzAPI.Frontend && npm i && npm run dev"
+start cmd /k "cd PizzAPI.E2E && npm i && npm run cy:run"
